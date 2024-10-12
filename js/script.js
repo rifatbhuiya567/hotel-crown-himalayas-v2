51a9accf77992f6934331a658ca3e06ae4295395
Rifat Bhuiya <113363474+rifatbhuiya567@users.noreply.github.com>
2024-10-12 19:12:30 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+(function ($) {
+    "use strict";
+
+    $(window).on('scroll', function () {
+        let scrolling = $(this).scrollTop();
+
+        if (scrolling > 50) {
+            $('.main-header').addClass('fixed-header');
+        } else {
+            $('.main-header').removeClass('fixed-header');
+        }
+
+        if (scrolling > 150) {
+            $('.btt').show(500);
+        } else {
+            $('.btt').hide(500);
+        }
+    });
+    
+    $('.menu-nav-btn').on('click', function () {
+        $('.menu-nav-btn').toggleClass('open');
+        $('.main-header .sidebar').toggle(500);
+    });
+
+    $('.btt').on('click', function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500);
+    });
+})(jQuery);
